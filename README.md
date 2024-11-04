15f7223efc567f43262113d9e9ab119f26402b9a
heiyt <heiyt.24h@gmail.com>
2024-11-04 15:17:41 +0800

    repair
    重新上传一遍，新增悔棋功能

--- README.md
+++
@@ -1,2 +0,0 @@
-# chinachess_2
- heiyt做的中国象棋人机对战版
--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_chinachess_2_FILE D:/Projects/ClionProjects/chinachess_2/cmake-build-debug/chinachess_2.exe)
-set(__QT_DEPLOY_TARGET_chinachess_2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_chinachess_2_RUNTIME_DLLS D:/Program_Data/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;D:/Program_Data/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;D:/Program_Data/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
